780118304220c5e893acb4aebf7ad98a3a8c6520
Tunan10 <tunansakib200@gmail.com>
2026-02-22 04:50:52 +0600

    compliting button toggle system

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ let total = document.getElementById('total-count');
 let interviewCount = document.getElementById('interview-count');
 let rejectCount = document.getElementById('rejected-count');
 const allcardSection = document.getElementById('All-job-card');
-console.log(allcardSection.children.length);
+const mainContainer = document.querySelector('main');
+
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
 
 function calculateCOunt() {
   total.innerText = allcardSection.children.length;
@@ -13,3 +17,23 @@ function calculateCOunt() {
   rejectCount.innerText = rejectedList.length;
 }
 calculateCOunt();
+function togglestyle(id) {
+  allFilterBtn.classList.remove('bg-blue-500', 'text-white');
+  interviewFilterBtn.classList.remove('bg-blue', 'text-white');
+  rejectedFilterBtn.classList.remove('bg-black', 'text-white');
+
+  allFilterBtn.classList.add(
+    'bg-white',
+    'text-gray-500',
+    'shadow-slate-200',
+    'border',
+    'border-gray-300',
+  );
+  interviewFilterBtn.classList.add('bg-white', 'text-gray-500');
+  rejectedFilterBtn.classList.add('bg-white', 'text-gray-500');
+
+  const selected = document.getElementById(id);
+  console.log(selected);
+  selected.classList.remove('bg-white', 'text-gray-500');
+  selected.classList.add('bg-blue-500', 'text-white');
+}
